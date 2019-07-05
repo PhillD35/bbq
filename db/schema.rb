c0f6060f343e17a9fb66a96aeb716c767340971a
Philld35 <mppodk@mail.ru>
2019-07-05 20:50:45 +0500

    pincode for events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_22_151200) do
+ActiveRecord::Schema.define(version: 2019_07_05_132527) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_06_22_151200) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "pincode"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
